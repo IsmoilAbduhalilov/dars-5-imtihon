0a7bcece4154c4f04144b9e4271758561e41ac36
abduxalilovismoil <abduhalilovaismoil@gmail.com>
2024-07-15 21:07:55 +0500

    fist commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var copy = document.querySelector(".sliderr").cloneNode(true);
+document.querySelector(".swipper").appendChild(copy);
+
+var copy = document.querySelector(".slider").cloneNode(true);
+document.querySelector(".swiper").appendChild(copy);
